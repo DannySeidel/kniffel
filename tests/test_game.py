26775d450e6b05933002a7e55b399e0314bdcef9
Danny Seidel <danny.seidel02@web.de>
2022-04-19 14:08:51 +0200

    added foundation for tests

---
+++ tests/test_game.py
@@ -0,0 +1,9 @@
+# pylint: disable=C
+# pylint: disable=protected-access
+
+import unittest
+
+
+class TestGame(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)  # add assertion here
---
+++ tests/test_player.py
@@ -0,0 +1,9 @@
+# pylint: disable=C
+# pylint: disable=protected-access
+
+import unittest
+
+
+class TestPlayer(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)  # add assertion here
