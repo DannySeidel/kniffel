DannySeidel/kniffel

commit 7e25d78cf789af701937b336c620cdec4fa7e011

des gehört da auch dazu

src/main.py
@@ -193,9 +193,13 @@ class Terminal:
                     player.reuse_dice()
                 for value in player.dice_used:
                     print(f"{Text.REGULAR}    Do you want rethrow the dice with current value {Text.SCORE + str(value) + Color.END}?")
-                    action = input(f"{Text.REGULAR}    Enter action [Y/N]: ")
-                    if action == ("n" or "N"):
+                    action = input(f"{Text.REGULAR}    Enter action [Y/N]:\n"
+                                   f"Enter 'K' to keep all dice: ")
+                    if action in ("n" or "N"):
                         player.put_dice_aside(value)
+                    elif action in ("k" or "K"):
+                        for value2 in player.dice_used:
+                            player.put_dice_aside(value2)
                 attempt += 1
             else:
                 for value in player.dice_used:

commit 4a24bb497dd01299313f1a87ea1450d6c089d3e1

fixed bug introduced in [7e25d78cf789af701937b336c620cdec4fa7e011]

src/main.py
@@ -195,9 +195,9 @@ class Terminal:
                     print(f"{Text.REGULAR}    Do you want rethrow the dice with current value {Text.SCORE + str(value) + Color.END}?")
                     action = input(f"{Text.REGULAR}    Enter action [Y/N]:\n"
                                    f"   Enter 'K' to keep all remaining dice: ")
-                    if action in ("n" or "N"):
+                    if action == ("n" or "N"):
                         player.put_dice_aside(value)
-                    elif action in ("k" or "K"):
+                    elif action == ("k" or "K"):
                         for value2 in player.dice_used:
                             player.put_dice_aside(value2)
                         break